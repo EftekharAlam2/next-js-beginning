b28e823077297eeef4dba56c49b7a44175b4bdff
EftekharAlam2 <hinsouvlmare@gmail.com>
2023-06-19 10:48:25 +0600

    route creating using folder

---
+++ src/app/about/nested/page.js
@@ -0,0 +1,14 @@
+export const metadata = {
+  title: "Next.js Practice | Nested Page",
+  description: "Next.js Practice",
+};
+
+const NestedPage = () => {
+  return (
+    <div>
+      <h1>Nested Page</h1>
+    </div>
+  );
+};
+
+export default NestedPage;
---
+++ src/app/about/page.js
@@ -0,0 +1,10 @@
+export const metadata = {
+  title: "Next.js Practice | About",
+  description: "Next.js Practice",
+};
+
+const AboutPage = () => {
+  return <div>About Page</div>;
+};
+
+export default AboutPage;
